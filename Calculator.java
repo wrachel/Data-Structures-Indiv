7aa87800b86ebaa17b4bae25706beee6ef206503
wrachel <Rwei951@gmail.com>
2022-03-31 09:32:59 -0700

    fixing menu &calculator

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.lang.Math;
 
 import java.util.Stack;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Calculator {
@@ -31,13 +31,8 @@ public class Calculator {
 
     //supported operators
     private final Map<String, Integer> OPERATORS = new HashMap<>();
-
     {
         // Map<"token", precedence>
-
-        //adding a sqrt operator
-        OPERATORS.put("SQRT", 2);
-
         //adding a power operator
         OPERATORS.put("^", 2);
 
@@ -50,7 +45,6 @@ public class Calculator {
 
     // Helper definition for supported operators
     private final Map<String, Integer> SEPARATORS = new HashMap<>();
-
     {
         // Map<"separator", not_used>
         SEPARATORS.put(" ", 0);
@@ -73,9 +67,8 @@ public class Calculator {
     // Compare precedence of operators.
     private Boolean isPrecedent(String token1, String token2) {
         // token 1 is precedent if it is greater than token 2
-        return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0);
+        return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0) ;
     }
-
 
     // Term Tokenizer takes original expression and converts it to ArrayList of tokens
     private void termTokenizer() {
@@ -86,7 +79,7 @@ public class Calculator {
         StringBuilder multiCharTerm = new StringBuilder();    // term holder
         for (int i = 0; i < this.expression.length(); i++) {
             Character c = this.expression.charAt(i);
-            if (isOperator(c.toString()) || isSeperator(c.toString())) {
+            if ( isOperator(c.toString() ) || isSeperator(c.toString())  ) {
                 // 1st check for working term and add if it exists
                 if (multiCharTerm.length() > 0) {
                     tokens.add(this.expression.substring(start, i));
@@ -112,7 +105,7 @@ public class Calculator {
     }
 
     // Takes tokens and converts to Reverse Polish Notation (RPN), this is one where the operator follows its operands.
-    private void tokensToReversePolishNotation() {
+    private void tokensToReversePolishNotation () {
         // contains final list of tokens in RPN
         this.reverse_polish = new ArrayList<>();
 
@@ -125,8 +118,9 @@ public class Calculator {
                     tokenStack.push(token);
                     break;
                 case ")":
-                    while (!tokenStack.empty() && !tokenStack.peek().equals("(")) {
-                        reverse_polish.add((String) tokenStack.pop());
+                    while (!tokenStack.empty() && !tokenStack.peek().equals("("))
+                    {
+                        reverse_polish.add( (String)tokenStack.pop() );
                     }
                     tokenStack.pop();
                     break;
@@ -136,13 +130,13 @@ public class Calculator {
                 case "/":
                 case "%":
                 case "^":
-                case "SQRT":
                     // While stack
                     // not empty AND stack top element
                     // and is an operator
-                    while (!tokenStack.empty() && isOperator((String) tokenStack.peek())) {
-                        if (isPrecedent(token, (String) tokenStack.peek())) {
-                            reverse_polish.add((String) tokenStack.pop());
+                    while (!tokenStack.empty() && isOperator((String) tokenStack.peek()))
+                    {
+                        if ( isPrecedent(token, (String) tokenStack.peek() )) {
+                            reverse_polish.add((String)tokenStack.pop());
                             continue;
                         }
                         break;
@@ -156,120 +150,101 @@ public class Calculator {
         }
         // Empty remaining tokens
         while (!tokenStack.empty()) {
-            reverse_polish.add((String) tokenStack.pop());
+            reverse_polish.add((String)tokenStack.pop());
         }
 
     }
 
     // Takes RPN and produces a final result
-    private void rpnToResult() {
+    private void rpnToResult()
+    {
         // Stack used to hold calculation while process RPN
         Stack calculation = new Stack();
 
         // for loop to process RPN
-        for (String a : this.reverse_polish) {
-            if (!isOperator(a))// If the token is a number
+        for(String a : this.reverse_polish){
+            if(!isOperator(a))// If the token is a number
             {
                 calculation.push(a); // Push number to stack
                 //System.out.println("Enqueued: "+ a);
-            } else// if the token is not a number
-            {
-                Double result = 0.0;
-                if (a.equals("SQRT")) {
-
-                    Double Entry = Double.valueOf((String) calculation.pop());
-                    result = Math.pow(0.5, Entry);
-
-
-                    /*
-                    if (Entry == 0 || Entry == 1) {
-                        returnValue = Entry;
-                    } else {
-                        int i = 1, j = 1;
-
-                        //Starting from 1, try all numbers until i*i is greater than or equal to a
-                        //returns i - 1
-                        while (j <= Entry) {
-                            i++;
-                            j = i * i;
-                        }
-                        result = Double.valueOf(i - 1);
-                    }
-
-                     */
-                } else {
-                    Double topEntry = Double.valueOf((String) calculation.pop());// Pop the two top entries
-                    Double nextEntry = Double.valueOf((String) calculation.pop());
-
-                    // Based off of Token operator calculate result
-
-                    if (a.equals("+")) {
-                        result = nextEntry + topEntry;
-                        //System.out.println("Result: " + result);
-                    } else if (a.equals("-")) {
-                        result = nextEntry - topEntry;
-                        //System.out.println("Result: " + result);
-                    } else if (a.equals("*")) {
-                        result = nextEntry * topEntry;
-                        //System.out.println("Result: " + result);
-                    } else if (a.equals("/")) {
-                        result = nextEntry / topEntry;
-                        //System.out.println("Result: " + result);
-                    } else if (a.equals("%")) {
-                        result = nextEntry % topEntry;
-                        //System.out.println("Result: " + result);
-                    } else if (a.equals("^")) {
-                        Double value = nextEntry;
-                        for (int i = 0; i < topEntry - 1; i++) {
-                            value *= nextEntry;
-                        }
-                        result = value;
-                    }
-                    //System.out.println("Result: " + result);
-                    calculation.push(String.valueOf(result)); // Push result back onto the stack
-                }
             }
-            // Pop final result and set as final result for expression
-            this.result = Double.valueOf((String) calculation.pop());
+            else// else
+            {
+                Double topEntry = Double.valueOf((String) calculation.pop());// Pop the two top entries
+                Double nextEntry = Double.valueOf((String) calculation.pop());
+
+                // Based off of Token operator calculate result
+                Double result = 0.0; //create variable to store the resulting calculation
+                if(a.equals("+")){
+                    result = nextEntry + topEntry;
+                    //System.out.println("Result: " + result);
+                }
+                else if(a.equals("-")){
+                    result = nextEntry - topEntry;
+                    //System.out.println("Result: " + result);
+                }
+                else if(a.equals("*")){
+                    result = nextEntry * topEntry;
+                    //System.out.println("Result: " + result);
+                }
+                else if(a.equals("/")){
+                    result = nextEntry / topEntry;
+                    //System.out.println("Result: " + result);
+                }
+                else if(a.equals("%")) {
+                    result = nextEntry % topEntry;
+                    //System.out.println("Result: " + result);
+                }
+                else if(a.equals("^")){
+                    Double value = nextEntry;
+                    for(int i = 0; i < topEntry - 1; i++){
+                        value *= nextEntry;
+                    }
+                    result = value;
+                }
+                //System.out.println("Result: " + result);
+                calculation.push(String.valueOf(result)); // Push result back onto the stack
+            }
         }
+        // Pop final result and set as final result for expression
+        this.result = Double.valueOf((String) calculation.pop());
     }
 
-        // Print the expression, terms, and result
-        public String toString() {
-            return ("Original expression: " + this.expression + "\n" +
-                    "Tokenized expression: " + this.tokens.toString() + "\n" +
-                    "Reverse Polish Notation: " + this.reverse_polish.toString() + "\n" +
-                    "Final result: " + String.format("%.2f", this.result) + "\n");
-        }
-
-        //tester function
-        public static void main (String[]args){
-            Calculator simpleMath = new Calculator("100 + 200  * 3");
-            System.out.println("Simple Math\n" + simpleMath);
-
-            Calculator parenthesisMath = new Calculator("(100 + 200)  * 3");
-            System.out.println("Parenthesis Math\n" + parenthesisMath);
-
-            Calculator allMath = new Calculator("200 % 300 + 5 + 300 / 200 + 1 * 100");
-            System.out.println("All Math\n" + allMath);
-            Calculator allMath2 = new Calculator("200 % (300 + 5 + 300) / 200 + 1 * 100");
-            System.out.println("All Math2\n" + allMath2);
-
-            System.out.print("POWER CHALLENGE: ");
-            Calculator power = new Calculator("2 ^ 3");
-            System.out.println("power\n" + power);
-
-
-            Scanner scanner = new Scanner(System.in);
-
-            //get user input for mathematical expression
-            System.out.println("Input a mathematical expression to evaluate: ");
-            String input = scanner.nextLine();
-
-            Calculator newInput = new Calculator(input);
-            System.out.println("User input\n" + newInput);
-
-
-        }
+    // Print the expression, terms, and result
+    public String toString() {
+        return ("Original expression: " + this.expression + "\n" +
+                "Tokenized expression: " + this.tokens.toString() + "\n" +
+                "Reverse Polish Notation: " +this.reverse_polish.toString() + "\n" +
+                "Final result: " + String.format("%.2f", this.result) + "\n");
     }
 
+    //tester function
+    public static void main(String[] args){
+        Calculator simpleMath = new Calculator("100 + 200  * 3");
+        System.out.println("Simple Math\n" + simpleMath);
+
+        Calculator parenthesisMath = new Calculator("(100 + 200)  * 3");
+        System.out.println("Parenthesis Math\n" + parenthesisMath);
+
+        Calculator allMath = new Calculator("200 % 300 + 5 + 300 / 200 + 1 * 100");
+        System.out.println("All Math\n" + allMath);
+        Calculator allMath2 = new Calculator("200 % (300 + 5 + 300) / 200 + 1 * 100");
+        System.out.println("All Math2\n" + allMath2);
+
+
+        Calculator power = new Calculator("2 ^ 3");
+        System.out.println("power\n" + power);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        //get user input for mathematical expression
+        System.out.println("Input a mathematical expression to evaluate: ");
+        String input = scanner.nextLine();
+
+        Calculator newInput = new Calculator(input);
+        System.out.println("User input\n" + newInput);
+
+
+    }
+}
--- Main.java
+++ Main.java
@@ -4,62 +4,62 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.RunnableFuture;
 
-
-public class Main {  
+public class Main {
     String title;
     Runnable action;
 
     public Main(String title, Runnable action) {
-        //constructor
+        // constructor
         this.title = title;
         this.action = action;
     }
 
     public String getTitle() {
-        return this.title; //getter for title
+        return this.title; // getter for title
     }
 
     public Runnable getAction() {
-        return this.action; //getter for the action that should be run
+        return this.action; // getter for the action that should be run
     }
 
     public static void main(String[] args) {
-      
+
         Scanner sc = new Scanner(System.in);
 
-        //Hash map to put the different options for the menu
+        // Hash map to put the different options for the menu
         Map<Integer, Main> menu = new HashMap<>();
 
-      //menu options
-        menu.put(1, new Main("TT0 Challenge: IntByReference", () -> IntByReference.main(null) ) );
-        menu.put(2, new Main("TT0 Challenge: Matrix", () -> Matrix.main(null) ) );
+        // menu options
+        menu.put(1, new Main("TT0 Challenge: IntByReference", () -> IntByReference.main(null)));
+        menu.put(2, new Main("TT0 Challenge: Matrix", () -> Matrix.main(null)));
         menu.put(3, new Main("TT1 Challenge: Queue Iterator", () -> QueueTester.main(null)));
-        menu.put(4, new Main("TT1 Challenge: Merge Queues", ()-> MergingQueues.main(null)));
-        menu.put(5, new Main("TT1 Challenge: Reverse Queues", ()-> reverseQueues.main(null)));
+        menu.put(4, new Main("TT1 Challenge: Merge Queues", () -> MergingQueues.main(null)));
+        menu.put(5, new Main("TT1 Challenge: Reverse Queues", () -> reverseQueues.main(null)));
+        menu.put(6, new Main("TT2 Challenges: Calculator", () -> Calculator.main(null)));
 
-        //prints the menu
+        // prints the menu
         System.out.println("Menu:");
 
         for (Map.Entry<Integer, Main> pair : menu.entrySet()) {
             System.out.println(pair.getKey() + " - " + pair.getValue().getTitle());
         }
 
-        //scanner gets user input for menu selection
+        // scanner gets user input for menu selection
         int input = sc.nextInt();
 
         try {
-            //according to user input, return the method at the Map's index
+            // according to user input, return the method at the Map's index
             Main m = menu.get(input);
-            //run action
+            // run action
             m.getAction().run();
             System.out.println("");
 
-        } catch (Exception e){
-            //exception for if user puts something not applicable
+        } catch (Exception e) {
+            // exception for if user puts something not applicable
             System.out.println("Something went wrong. Please try again. \n");
         }
 
-        //recursive statement to get menu to run again
+        // recursive statement to get menu to run again
         Main.main(args);
     }
 
